5008b4e8e30ee63af01a397f8d2dd7ca740ce631
Furkan Aytan <aytan@editsystems.com>
2022-03-20 23:10:03 +0300

    Send collider to the bounceball function

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -15,10 +15,10 @@ public class BallController : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D other) {
         // Check if the collider is one of the paddles, player | enemy
         if(other!=null){
-            BounceBall();
+            BounceBall(other);
         }
     }
-    private void BounceBall(){
+    private void BounceBall(Collision2D other){
             dist = other.transform.position.x - transform.position.x;
             // divided by absolute to send the ball to correct position.
             ballRb.AddForce(transform.right * -1 * dist/(Mathf.Abs(dist)) * ballSpeed );
